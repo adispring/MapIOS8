d5ed6c2123c893230e920403daa6b7be302632d9
adispring <sunnyadi@163.com>
2014-12-11 11:15:05 +0800

    Initial Commit

---
+++ MapIOS8Tests/MapIOS8Tests.m
@@ -0,0 +1,40 @@
+//
+//  MapIOS8Tests.m
+//  MapIOS8Tests
+//
+//  Created by 王增迪 on 12/11/14.
+//  Copyright (c) 2014 Wang Zengdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MapIOS8Tests : XCTestCase
+
+@end
+
+@implementation MapIOS8Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
